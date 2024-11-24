775e5ab953944febc46261c23bf2fd9e2f6bab5a
ketutezraugm <iketutezrahesperosmaska@mail.ugm.ac.id>
2024-11-24 18:05:25 +0700

    Create test_square.cpp

---
+++ Makefile/test_square.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int square(int a, int b) {
+    return (a*b);
+}
+
+int main() {
+    int a=5;
+    int b=6;
+    
+    cout << square(a,b);
+    
+    return 0;
+}
